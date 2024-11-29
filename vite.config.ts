49c94b6cf39f589f938cad7c0bd6a332948fc9d1
DESKTOP-3A7BJ1J\THINKPAD <t2gportal@gmail.com>
2024-11-29 16:17:37 +0700

    Config path src - init source shopee clone

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '~': path.resolve(__dirname, './src')
+      src: path.resolve(__dirname, './src')
     }
   }
-});
+})
